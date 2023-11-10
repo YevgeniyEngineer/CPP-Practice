e0063fa32d137ed4764dc342a385e43c847efce3
Yevgeniy <yevgeniy.engineer@gmail.com>
2023-11-10 20:51:21 +0800

    Added filter by make

--- src/ranges.cpp
+++ src/ranges.cpp
@@ -1,6 +1,9 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <iterator>
+#include <ostream>
+#include <ranges>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -8,15 +11,35 @@
 #include "read_csv.hpp"
 #include "vehicle.hpp"
 
+std::ostream &operator<<(std::ostream &os, const std::vector<Vehicle> &vehicles)
+{
+    for (const auto &vehicle : vehicles)
+    {
+        os << vehicle << '\n';
+    }
+
+    return os;
+}
+
+// Filter vehicles by make
+void filterByMake(const std::vector<Vehicle> &vehicles, const std::string &make)
+{
+    std::cout << "Vehicles of make " << make << ":\n";
+
+    auto filtered =
+        vehicles | std::views::filter([&make](const Vehicle &vehicle) noexcept { return (vehicle.make == make); });
+
+    std::cout << std::vector<Vehicle>(std::make_move_iterator(filtered.begin()),
+                                      std::make_move_iterator(filtered.end()))
+              << std::endl;
+}
+
 int main()
 {
     const std::string filename = "../data/vehicles_dataset.csv";
     const std::vector<Vehicle> vehicles = readCSV(filename);
 
-    for (const auto &vehicle : vehicles)
-    {
-        std::cout << vehicle << std::endl;
-    }
+    filterByMake(vehicles, "Toyota");
 
     return 0;
 }
